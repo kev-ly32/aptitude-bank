1d8138212e1715ae9e97288b82b2d6e400cfa28c
Kevin Ly <kev_ly32@hotmail.com>
2020-10-23 21:34:03 -0400

    updated dashboard template and styling

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -12,13 +12,30 @@ function Dashboard(props) {
       </section>
       <section className="dashboard-main">
         <section className="dashboard-cta">
-          <button>Pay Bill</button>
-          <button>Send e-Transfer</button>
-          <button>Investments</button>
-          <button>Transfer money between accounts</button>
+          <button className="dashboard-button">Pay Bill</button>
+          <button className="dashboard-button">Send e-Transfer</button>
+          <button className="dashboard-button">Investments</button>
+          <button className="dashboard-button">
+            Transfer money between accounts
+          </button>
         </section>
         <section className="dashboard-content">
-          <div>ACCOUNTS</div>
+          <h1 className="accounts-header">EVERYDAY ACCOUNTS</h1>
+          <div className="account">
+            <h3 className="account-type">
+              Everyday Savings
+              <span className="account-number">1678 3828394</span>
+            </h3>
+            <h4 className="account-balance">$10,500.53</h4>
+          </div>
+          <h1 className="accounts-header">INVESTMENTS</h1>
+          <div className="account">
+            <h3 className="account-type">
+              TFSA
+              <span className="account-number">1678 3824628</span>
+            </h3>
+            <h4 className="account-balance">$5,288.93</h4>
+          </div>
         </section>
       </section>
     </div>
