1a5f8d2fdb6077448b089fad4253fa97e9673165
Kevin Ly <kev_ly32@hotmail.com>
2020-10-25 16:26:34 -0400

    configured express-session and passport

--- app.js
+++ app.js
@@ -7,24 +7,29 @@ const express = require("express"),
   path = require("path"),
   port = process.env.PORT || 5000;
 
+const User = require("./models/User");
+
 //Initialize express parser (body parser) to parse our requests to the server
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// //Configure passport
-// app.use(
-//   session({
-//     secret: process.env.SECRET || "the rabbit is in the hat",
-//     resave: false,
-//     saveUninitialized: false,
-//   })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
+//Configure express-session
+app.use(
+  session({
+    secret: process.env.SECRET || "the rabbit is in the hat",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-// passport.use(new LocalStrategy(User.authenticate()));
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
+//Configure passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Configure passport-local-mongoose
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/credit-cards", (req, res) => {
   res.json({ cards: "look at all the credit cards" });
