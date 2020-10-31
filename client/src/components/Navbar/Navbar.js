619c8af030763ee33ed93cba6990bec0935688c2
Kevin Ly <kev_ly32@hotmail.com>
2020-10-30 23:01:10 -0400

    added conditional logic for logout button

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "../../public/stylesheets/Navbar.css";
+import { selectUser } from "../../reducers/Authentication/userSlice";
 
-function Navbar(props) {
+function Navbar() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="navbar">
       <div className="navbar-container">
@@ -10,18 +14,26 @@ function Navbar(props) {
           Aptitude Bank
         </Link>
         <nav>
-          <ul className="nav-links">
-            <li className="nav-link">
-              <Link to="/login">
-                <button className="login-button">Login</button>
-              </Link>
-            </li>
-            <li className="nav-link">
-              <Link to="/register">
-                <button className="register-button">Sign Up</button>
-              </Link>
-            </li>
-          </ul>
+          {!user ? (
+            <ul className="nav-links">
+              <li className="nav-link">
+                <Link to="/login">
+                  <button className="login-button">Login</button>
+                </Link>
+              </li>
+              <li className="nav-link">
+                <Link to="/register">
+                  <button className="register-button">Sign Up</button>
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className="nav-links">
+              <li className="nav-link">
+                <button className="login-button">Logout</button>
+              </li>
+            </ul>
+          )}
         </nav>
       </div>
     </div>
